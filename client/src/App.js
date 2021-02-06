4443e69e82794c816a885535d5114afa25c75fc4
Skyler Dakota Lawton <skylerdakota@gmail.com>
2021-02-06 10:15:05 -0500

    updated router on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import Books from "./pages/Books";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
 import Nav from "./components/Nav";
+
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Books />
-    </div>
+    <Router>
+      <Switch>
+        <div>
+          <Nav />
+          <Route exact path="/" component={Search} />
+          <Route exact path="/about" component={Search} />
+          <Route exact path="/contact" component={Saved} />
+          <Books />
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
